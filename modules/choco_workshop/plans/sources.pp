07da29be8cc2fc1253a80d1446fa1570c5935b72
Matthew Stone <matt@souldo.net>
2020-04-29 07:16:48 -0500

    Update sources.pp

--- modules/choco_workshop/plans/sources.pp
+++ modules/choco_workshop/plans/sources.pp
@@ -2,7 +2,7 @@ plan choco_workshop::sources() {
   apply('localhost') {
     chocolateysource { 'internal' :
           ensure   => present,
-          location => "http://localhost:8081/repository/choco/",
+          location => "http://localhost:8081/repository/internalrepo/",
     }
     chocolateysource { 'chocolatey' : ensure => absent }
   }
